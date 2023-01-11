b09d9de999f2df23920e31e7ded2f4137dc6d59d
ZachJHansen <zachansen62@gmail.com>
2023-01-11 12:42:49 -0600

    Adding conditional literals for heads of rules

--- program.py
+++ program.py
@@ -41,6 +41,38 @@ def terminator(spec, next_rtype):
         spec = spec + ".\n"
         return spec
 
+# Returns a list of tuples: preceding char, literal, following char
+def process_head(line):
+    literals = []
+    if line is None:
+        return literals
+    line = line.strip("\n").strip(".").strip()
+    cl = re.search(r'^{(.+):(.+)}$', line)
+    if cl:                                                      # { p(X) : q(X),...,t(X) }
+        head, body = cl.group(1), cl.group(2).split("%")
+        literals.append(("{", head, ":"))
+        for i in range(len(body)-1):
+            literals.append((None, body[i], None))
+        literals.append((None, body[-1], "}"))
+        return literals
+    choice = re.search(r'^{(.+)}$', line)                       # { p(X) }
+    if choice:
+        head = choice.group(1)
+        literals.append(("{", head, "}"))
+        return literals
+    literals.append((None, line, None))
+    #print("Head literals:", literals)
+    return literals
+
+def process_body(line):
+    literals = []
+    if line is None:
+        return literals
+    else:
+        for l in line.split("%"):
+            literals.append((None, l, None))
+    return literals
+
 # Intervals, integers, symbolic constants, and integers are leaf terms
 # Unary operations are terms with one child, binary operations have two
 class Term:
@@ -72,15 +104,24 @@ class Atom:
             print("Fatal error.")
             sys.exit(1)
 
-# Literals are either positive or default negated (sign)
+# Literals are either positive, negative, or doubly negated (sign)
 # An atomic literal has one child (an atom)
+# A literal can be preceded by a {, and followed by a } or :
 class Literal:
-    def __init__(self, name, raw, sign, literal_type):
+    def __init__(self, name, raw, sign, literal_type, prec_char, foll_char):
         self.name = name
         self.raw = raw
         self.sign = sign
         self.child = None
         self.type = literal_type
+        if prec_char is None:
+            self.preceding_char = ''
+        else:
+            self.preceding_char = prec_char
+        if foll_char is None:   
+            self.following_char = ''
+        else:
+            self.following_char = foll_char
 
 
 # A program is a list of lines
@@ -149,6 +190,24 @@ class Program:
         term_counter = 0
         line = re.sub(r':-', "#", line)       
 
+        bin_op_matches = re.findall(r'\w+ *[\+\-\*\\\/] *\w+', line)
+        for match in bin_op_matches:
+            term_counter += 1
+            name = "_l" + str(line_number) + "t" + str(term_counter) + "_"
+            decompose = re.search(r'(\w+ *)([\+\-\*\\\/])( *\w+)', match)
+            term = Term(name, match, [decompose.group(1), decompose.group(3)], decompose.group(2))
+            self.terms.append(term)
+            line = line.replace(term.raw, term.name)
+
+        unary_op_matches = re.findall(r'-\w+', line)
+        for match in unary_op_matches:
+            term_counter += 1
+            name = "_l" + str(line_number) + "t" + str(term_counter) + "_"
+            decompose = re.search(r'(-)(\w+)', match)
+            term = Term(name, match, [decompose.group(1)], decompose.group(2))
+            self.terms.append(term)
+            line = line.replace(term.raw, term.name)
+
         aim1 = [re.sub(r'[\+\-\*\\\/] *', "", match).strip() for match in re.findall(r'[\+\-\*\\\/] *\([^\)]+\)', line)]
         aim2 = [re.sub(r'[\+\-\*\\\/] *', "", match).strip() for match in re.findall(r'\([^\)]+\) *[\+\-\*\\\/]', line)]
         arithmetic_interval_matches = aim1 + aim2
@@ -164,60 +223,56 @@ class Program:
             term = Term("_l" + str(line_number) + "t" + str(term_counter) + "_", match)
             self.terms.append(term)
             line = line.replace(term.raw, term.name)
-
-        bin_op_matches = re.findall(r'\w+ *[\+\-\*\\\/] *\w+', line)
-        for match in bin_op_matches:
-            term_counter += 1
-            name = "_l" + str(line_number) + "t" + str(term_counter) + "_"
-            decompose = re.search(r'(\w+ *)([\+\-\*\\\/])( *\w+)', match)
-            term = Term(name, match, [decompose.group(1), decompose.group(3)], decompose.group(2))
-            self.terms.append(term)
-            line = line.replace(term.raw, term.name)
-
-        unary_op_matches = re.findall(r'-\w+', line) 
-        for match in unary_op_matches:
-            term_counter += 1
-            name = "_l" + str(line_number) + "t" + str(term_counter) + "_"
-            decompose = re.search(r'(-)(\w+)', match)
-            term = Term(name, match, [decompose.group(1)], decompose.group(2))
-            self.terms.append(term)
-            line = line.replace(term.raw, term.name)
         return line
 
     def find_literals(self, line_number, line):
         literal_objects = []
+        literal_candidates = []
         tup_match = re.findall('\([^\)]+\)', line)                  # Temporarily replace commas within argument lists with @ special character
         replacements = [re.sub(",", "@", m) for m in tup_match]
         for i, m in enumerate(tup_match):
             line = line.replace(m, replacements[i], 1)
         line = line.replace(",", "%")                               # Replace all remaining commas with a special character to denote literals demarkation
-        literals = re.split(r'%|#', line)
-        atomic_literals = [l.strip("\t") for l in literals if re.search("<|>|<=|>=|=|!=", l) is None]
-        arithmetic_literals = [l.strip("\t") for l in literals if re.search("<|>|<=|>=|=|!=", l) is not None]
+        if re.search(r'#', line):
+            rule_parts = line.split("#")
+            if len(rule_parts) > 1:
+                head, body = rule_parts
+            else:
+                head, body = None, ruleparts[0]
+        else:
+            head, body = line, None
+        head_literals = process_head(head)
+        body_literals = process_body(body)
+        for l in head_literals:
+            literal_candidates.append(l)
+        for l in body_literals:
+            literal_candidates.append(l)
         lit_counter = 0
-        for lit in atomic_literals:
-            if lit and lit.strip():
+        for tup in literal_candidates:
+            lit = tup[1].strip("\t").strip()
+            if lit:                                                                         # Empty check
                 name = "_l" + str(line_number) + "l" + str(lit_counter) + "_"
-                if re.search(r'\Wnot\W', lit):
-                    lit = re.sub(r'\Wnot\W', "", lit)
-                    lit = Literal(name, lit, "negative", "atomic")
-                else:
-                    lit = Literal(name, lit, "positive", "atomic")
+                if re.search(r'^not not \w', lit):                                          # Doubly negated atom
+                    if re.search("<|>|<=|>=|=|!=", lit) is None:                                # Atomic literals
+                        lit = re.sub(r'^not not ', "", lit)
+                        lit = Literal(name, lit, "double negative", "atomic", tup[0], tup[2])
+                    else:                                                                       # Arithmetic literals
+                        lit = re.sub(r'^not not ', "", lit)
+                        lit = Literal(name, lit, "double negative", "arithmetic", tup[0], tup[2])
+                elif re.search(r'^not \w', lit):                                            # Singly negated atom
+                    if re.search("<|>|<=|>=|=|!=", lit) is None:                                # Atomic literals
+                        lit = re.sub(r'^not ', "", lit)
+                        lit = Literal(name, lit, "negative", "atomic", tup[0], tup[2])
+                    else:                                                                       # Arithmetic literals
+                        lit = re.sub(r'^not ', "", lit)
+                        lit = Literal(name, lit, "negative", "arithmetic", tup[0], tup[2])
+                else:                                                                       # Positive atom
+                    if re.search("<|>|<=|>=|=|!=", lit) is None:                                # Atomic literals
+                        lit = Literal(name, lit, "positive", "atomic", tup[0], tup[2])
+                    else:                                                                       # Arithmetic literals
+                        lit = Literal(name, lit, "positive", "arithmetic", tup[0], tup[2])
+                lit_counter += 1
                 literal_objects.append(lit)
-            else:
-                pass
-            lit_counter += 1
-        for lit in arithmetic_literals:
-            if lit and lit.strip():
-                name = "_l" + str(line_number) + "l" + str(lit_counter) + "_"
-                if re.search(r'\Wnot\W', lit):
-                    lit = re.sub(r'\Wnot\W', "", lit)
-                    lit = Literal(name, lit, "negative", "arithmetic")
-                else:
-                    lit = Literal(name, lit, "positive", "arithmetic")
-                literal_objects.append(lit)
-            else:
-                pass
         self.literals.append(literal_objects)
         return line
 
@@ -239,7 +294,7 @@ class Program:
                                         term = t.raw
                             arg_list += term + ","
                         term = literal.child.arguments[-1]
-                        for i in range(3):
+                        for i in range(4):
                             for t in self.terms:
                                 if term == t.name:
                                     term = t.raw
@@ -247,23 +302,45 @@ class Program:
                     else:
                         arg_list = ""
                     atom = literal.child.pname + arg_list
-                    if literal.sign == "negative":
+                    line += literal.preceding_char
+                    if literal.sign == "double negative":
+                        line += " not not " + atom
+                    elif literal.sign == "negative":
                         line += " not " + atom
                     else:
                         line += atom
+                    line += literal.following_char
                 else:
-                    if literal.sign == "negative":
+                    line += literal.preceding_char
+                    if literal.sign == "double negative":
+                        line += " not not " + literal.raw.strip(".")
+                    elif literal.sign == "negative":
                         line += " not " + literal.raw.strip(".")
                     else:
                         line += literal.raw.strip(".")
-                    for t in self.terms:
-                        if re.search(t.name, line):
-                            line = line.replace(t.name, t.raw)
-
+                    line += literal.following_char
+                    for i in range(4):
+                        for t in self.terms:
+                            if re.search(t.name, line):
+                                line = line.replace(t.name, t.raw)
                 if literal_counter == 0 and self.rule_types[line_counter] == 1:     # Head :- Body
+                    if literal.following_char == ":":                               # Head has form { p(X) : q(X) ... }
+                        print("Hello: ", line)
+                        pass
+                    else:
+                        line += " :- "
+                elif literal.following_char == "}" and self.rule_types[line_counter] == 1:
                     line += " :- "
                 else:
-                    line += ","
+                    if literal.following_char == ":":                               # Head has form { p(X) : q(X) ... }
+                        print("Hello: ", line)
+                        pass
+                    else:
+                        line += ","
+            for i in range(3):
+                for t in self.terms:
+                    if re.search(t.name, line):
+                        line = line.replace(t.name, t.raw)
             if line and line.strip():
                 if line_counter == len(self.literals)-1:
                     line = terminator(line, 1)
