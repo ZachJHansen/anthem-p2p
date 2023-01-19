fb588b49a7ed43d5c78b2db6b44919669042e2b4
ZachJHansen <zachansen62@gmail.com>
2023-01-19 12:05:46 -0700

    Update settings.py

--- web_anthem_p2p/web_anthem_p2p/settings.py
+++ web_anthem_p2p/web_anthem_p2p/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-))qk0#kinydg2-l!p@vcd&)ilh1*1=pk(i$((k!c3h!#!n6e3r'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
