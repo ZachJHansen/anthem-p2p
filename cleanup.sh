e48b8bc6ecebc61ddebaaf6cffb98366c2ff08d0
ZachJHansen <zachansen62@gmail.com>
2023-04-15 14:16:21 -0500

    Delete cleanup.sh

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove the completions text files, renamed files, etc.
-rm *completion.txt 
-rm *completion.spec 
-rm *-final.spec
-rm *renamed.lp 
-#rm new-*.lp 2>/dev/null
-rm new-*.lp 
